f269f0e86665999cb7e52b6fc9e34254144ee18c
MatayoshiMakoto <makotodev@gmail.com>
2015-03-06 22:20:40 -0300

    Add a utility category for layouting view. Add a round corner view to the category.

---
+++ FloripaTrans/UIView+FTViewLayout.m
@@ -0,0 +1,17 @@
+//
+//  UIView+FTViewLayout.m
+//  FloripaTrans
+//
+//  Created by Vítor Makoto Matayoshi de Moraes on 3/6/15.
+//  Copyright (c) 2015 Vitor Makoto. All rights reserved.
+//
+
+#import "UIView+FTViewLayout.h"
+
+@implementation UIView (FTViewLayout)
+
+- (void)roundViewCornerWithRadius:(int)radius {
+    self.layer.cornerRadius = 10;
+}
+
+@end
